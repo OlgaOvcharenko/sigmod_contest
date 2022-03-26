cb4d0cfd838054598cec1f8d0cebdc7ab784df6b
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2022-03-26 15:35:03 +0100

    Add high-value token-blocking

--- blocking.py
+++ blocking.py
@@ -1,79 +1,84 @@
 from collections import defaultdict
 from tqdm import tqdm
 import pandas as pd
+import numpy as np
 import re
 
 
-def block_with_attr(X, attr):  # replace with your logic.
-    '''
-    This function performs blocking using attr
-    :param X: dataframe
-    :param attr: attribute used for blocking
-    :return: candidate set of tuple pairs
-    '''
+def block_with_attr(X, id, attr):
+    tokens = dict()
+    doc_frequencies = dict()
 
-    # build index from patterns to tuples
-    pattern2id_1 = defaultdict(list)
-    pattern2id_2 = defaultdict(list)
+    # TODO normalization
+
+    num_words = 0
+
+    # tokenize records
     for i in tqdm(range(X.shape[0])):
         attr_i = str(X[attr][i])
-        pattern_1 = attr_i.lower()  # use the whole attribute as the pattern
-        pattern2id_1[pattern_1].append(i)
-
-        pattern_2 = re.findall("\w+\s\w+\d+", attr_i)  # look for patterns like "thinkpad x1"
-        if len(pattern_2) == 0:
+        pattern = re.findall("\w+\s\w+\d+", attr_i.lower())  # look for patterns like "thinkpad x1"
+        if len(pattern) == 0:
             continue
-        pattern_2 = list(sorted(pattern_2))
-        pattern_2 = [str(it).lower() for it in pattern_2]
-        pattern2id_2[" ".join(pattern_2)].append(i)
 
+        tokens[X[id][i]] = [pattern]
 
-    # add id pairs that share the same pattern to candidate set
-    candidate_pairs_1 = []
-    for pattern in tqdm(pattern2id_1):
-        ids = list(sorted(pattern2id_1[pattern]))
-        for i in range(len(ids)):
-            for j in range(i + 1, len(ids)):
-                candidate_pairs_1.append((ids[i], ids[j])) #
-    # add id pairs that share the same pattern to candidate set
-    candidate_pairs_2 = []
-    for pattern in tqdm(pattern2id_2):
-        ids = list(sorted(pattern2id_2[pattern]))
-        if len(ids)<100: #skip patterns that are too common
-            for i in range(len(ids)):
-                for j in range(i + 1, len(ids)):
-                    candidate_pairs_2.append((ids[i], ids[j]))
+        for token in pattern:
+            num_words += 1
+            frequency = doc_frequencies.get(token)
+            if frequency is not None:
+                doc_frequencies[token] = frequency + 1
+            else:
+                doc_frequencies[token] = 1
 
-    # remove duplicate pairs and take union
-    candidate_pairs = set(candidate_pairs_2)
-    candidate_pairs = candidate_pairs.union(set(candidate_pairs_1))
-    candidate_pairs = list(candidate_pairs)
+    # calculate tf-idf values
+    tf_idf_vec = np.zeros((len(doc_frequencies),)) # FIXME union with doc_frequencies
+    for i in tqdm(range(X.shape[0])):
+        sum, count = 0, 0
+        for token in tokens.get(X[id][i]):
+            tf = doc_frequencies.get(token) / num_words
+            idf = np.log(X.shape[0] / (doc_frequencies[token] + 1))
 
-    # sort candidate pairs by jaccard similarity.
-    # In case we have more than 1000000 pairs (or 2000000 pairs for the second dataset),
-    # sort the candidate pairs to put more similar pairs first,
-    # so that when we keep only the first 1000000 pairs we are keeping the most likely pairs
-    jaccard_similarities = []
-    candidate_pairs_real_ids = []
-    for it in tqdm(candidate_pairs):
-        id1, id2 = it
+            tf_idf = tf * idf
+            tf_idf_vec[token] = tf_idf
+            if tf_idf > 1:
+                sum += tf_idf
+                count += 1
 
-        # get real ids
-        real_id1 = X['id'][id1]
-        real_id2 = X['id'][id2]
-        if real_id1<real_id2: # NOTE: This is to make sure in the final output.csv, for a pair id1 and id2 (assume id1<id2), we only include (id1,id2) but not (id2, id1)
-            candidate_pairs_real_ids.append((real_id1, real_id2))
-        else:
-            candidate_pairs_real_ids.append((real_id2, real_id1))
+        tf_idf_avg = sum / count
+        tokens[X[id][i]] = tokens.get(X[id][i]).append([tf_idf_avg]) # FIXME one dict for all? Maybe lists with i
 
-        # compute jaccard similarity
-        name1 = str(X[attr][id1])
-        name2 = str(X[attr][id2])
-        s1 = set(name1.lower().split())
-        s2 = set(name2.lower().split())
-        jaccard_similarities.append(len(s1.intersection(s2)) / max(len(s1), len(s2)))
-    candidate_pairs_real_ids = [x for _, x in sorted(zip(jaccard_similarities, candidate_pairs_real_ids), reverse=True)]
-    return candidate_pairs_real_ids
+    # block values by their high value tokens
+    blocks = dict()
+    for i in tqdm(range(X.shape[0])):
+        for token in tokens.get(X[id][i]):
+            if tf_idf_vec[token] > tokens[X[id][i]][1][0]:
+                record_list = [str(X[attr][i]).lower()]
+
+                if token in blocks:
+                    record_list.extend(blocks.get(token))
+                blocks[token] = record_list
+
+    # improve block collection as an index, create index of weights for record pairs
+    weights = []
+    num_pairs, sum_weights = 0, 0
+    for block in blocks.keys():
+        block_records = blocks.get(block)
+        all_pairs = [(block_records[i], block_records[j]) for i in range(len(block_records)) for j in range(i+1, len(block_records))]
+
+        for r1, r2 in zip(all_pairs):
+            sim = len(r1.intersection(r2)) / max(len(r1), len(r2))  # Jaccard similarity
+            weights.append([r1, r2, sim])  # FIXME ids instead of records
+
+            num_pairs += 1
+            sum_weights += sim
+
+    weight_avg = sum_weights / num_pairs
+
+    for i in tqdm(range(weights.shape[0])):
+        if weights[i][2] < weight_avg:
+            weights.pop(i)
+
+    return weights
 
 
 def save_output(X1_candidate_pairs,
@@ -105,8 +110,8 @@ X1 = pd.read_csv("X1.csv")
 X2 = pd.read_csv("X2.csv")
 
 # perform blocking
-X1_candidate_pairs = block_with_attr(X1, attr="title")
-X2_candidate_pairs = block_with_attr(X2, attr="name")
+X1_candidate_pairs = block_with_attr(X1, id="id", attr="title")
+X2_candidate_pairs = block_with_attr(X2, id="id", attr="name")
 
 # save results
 save_output(X1_candidate_pairs, X2_candidate_pairs)
