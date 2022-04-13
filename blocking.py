279fa230e3f422c55a501dd3a4c76840db11af7e
philipportner <philipp.ortner@student.tugraz.at>
2022-04-13 16:38:47 +0200

    Refactor/documentation

--- blocking.py
+++ blocking.py
@@ -1,80 +1,17 @@
-from collections import defaultdict
-from tqdm import tqdm
 import pandas as pd
-import re
+import pdb
+from lsh import *
 
 
-def block_with_attr(X, attr):  # replace with your logic.
-    '''
-    This function performs blocking using attr
-    :param X: dataframe
-    :param attr: attribute used for blocking
-    :return: candidate set of tuple pairs
-    '''
-
-    # build index from patterns to tuples
-    pattern2id_1 = defaultdict(list)
-    pattern2id_2 = defaultdict(list)
-    for i in tqdm(range(X.shape[0])):
-        attr_i = str(X[attr][i])
-        pattern_1 = attr_i.lower()  # use the whole attribute as the pattern
-        pattern2id_1[pattern_1].append(i)
-
-        pattern_2 = re.findall("\w+\s\w+\d+", attr_i)  # look for patterns like "thinkpad x1"
-        if len(pattern_2) == 0:
-            continue
-        pattern_2 = list(sorted(pattern_2))
-        pattern_2 = [str(it).lower() for it in pattern_2]
-        pattern2id_2[" ".join(pattern_2)].append(i)
-
-
-    # add id pairs that share the same pattern to candidate set
-    candidate_pairs_1 = []
-    for pattern in tqdm(pattern2id_1):
-        ids = list(sorted(pattern2id_1[pattern]))
-        for i in range(len(ids)):
-            for j in range(i + 1, len(ids)):
-                candidate_pairs_1.append((ids[i], ids[j])) #
-    # add id pairs that share the same pattern to candidate set
-    candidate_pairs_2 = []
-    for pattern in tqdm(pattern2id_2):
-        ids = list(sorted(pattern2id_2[pattern]))
-        if len(ids)<100: #skip patterns that are too common
-            for i in range(len(ids)):
-                for j in range(i + 1, len(ids)):
-                    candidate_pairs_2.append((ids[i], ids[j]))
-
-    # remove duplicate pairs and take union
-    candidate_pairs = set(candidate_pairs_2)
-    candidate_pairs = candidate_pairs.union(set(candidate_pairs_1))
-    candidate_pairs = list(candidate_pairs)
-
-    # sort candidate pairs by jaccard similarity.
-    # In case we have more than 1000000 pairs (or 2000000 pairs for the second dataset),
-    # sort the candidate pairs to put more similar pairs first,
-    # so that when we keep only the first 1000000 pairs we are keeping the most likely pairs
-    jaccard_similarities = []
-    candidate_pairs_real_ids = []
-    for it in tqdm(candidate_pairs):
-        id1, id2 = it
-
-        # get real ids
-        real_id1 = X['id'][id1]
-        real_id2 = X['id'][id2]
-        if real_id1<real_id2: # NOTE: This is to make sure in the final output.csv, for a pair id1 and id2 (assume id1<id2), we only include (id1,id2) but not (id2, id1)
-            candidate_pairs_real_ids.append((real_id1, real_id2))
-        else:
-            candidate_pairs_real_ids.append((real_id2, real_id1))
-
-        # compute jaccard similarity
-        name1 = str(X[attr][id1])
-        name2 = str(X[attr][id2])
-        s1 = set(name1.lower().split())
-        s2 = set(name2.lower().split())
-        jaccard_similarities.append(len(s1.intersection(s2)) / max(len(s1), len(s2)))
-    candidate_pairs_real_ids = [x for _, x in sorted(zip(jaccard_similarities, candidate_pairs_real_ids), reverse=True)]
-    return candidate_pairs_real_ids
-
+#  X1_df = pd.read_csv("X1_large.csv")
+#  X2_df = pd.read_csv("X2_large.csv")
+X1_df = pd.read_csv("X1.csv")
+X2_df = pd.read_csv("X2.csv")
+#  l = logging.Logger("")
+#  h = logging.StreamHandler()
+#  f = logging.Formatter(fmt="[{filename}:{lineno}] {msg}", style="{")
+#  h.setFormatter(f)
+#  l.addHandler(h)
 
 def save_output(X1_candidate_pairs,
                 X2_candidate_pairs):  # save the candset for both datasets to a SINGLE file output.csv
@@ -89,24 +26,71 @@ def save_output(X1_candidate_pairs,
 
     # make sure to include exactly 1000000 pairs for dataset X1 and 2000000 pairs for dataset X2
     if len(X1_candidate_pairs) < expected_cand_size_X1:
-        X1_candidate_pairs.extend([(0, 0)] * (expected_cand_size_X1 - len(X1_candidate_pairs)))
+        X1_candidate_pairs.extend(
+            [(0, 0)] * (expected_cand_size_X1 - len(X1_candidate_pairs)))
     if len(X2_candidate_pairs) < expected_cand_size_X2:
-        X2_candidate_pairs.extend([(0, 0)] * (expected_cand_size_X2 - len(X2_candidate_pairs)))
+        X2_candidate_pairs.extend(
+            [(0, 0)] * (expected_cand_size_X2 - len(X2_candidate_pairs)))
 
-    all_cand_pairs = X1_candidate_pairs + X2_candidate_pairs  # make sure to have the pairs in the first dataset first
-    output_df = pd.DataFrame(all_cand_pairs, columns=["left_instance_id", "right_instance_id"])
+    # make sure to have the pairs in the first dataset first
+    all_cand_pairs = X1_candidate_pairs + X2_candidate_pairs
+    output_df = pd.DataFrame(all_cand_pairs, columns=[
+                             "left_instance_id", "right_instance_id"])
     # In evaluation, we expect output.csv to include exactly 3000000 tuple pairs.
     # we expect the first 1000000 pairs are for dataset X1, and the remaining pairs are for dataset X2
     output_df.to_csv("output.csv", index=False)
 
 
-# read the datasets
-X1 = pd.read_csv("X1.csv")
-X2 = pd.read_csv("X2.csv")
+def blocking_step(X):
+    # TODO: split into #hyperthreads jobs
 
-# perform blocking
-X1_candidate_pairs = block_with_attr(X1, attr="title")
-X2_candidate_pairs = block_with_attr(X2, attr="name")
+    # %%
+    k = 3  # ~5 for small docs (emails), 9 - 10 for large docs(papers)
+    shingles = []
+    for row in X:
+        id, _, data = row.partition(' ')
+        shingles.append((id, k_shingles(data, k)))
 
-# save results
-save_output(X1_candidate_pairs, X2_candidate_pairs)
+    # %%
+    all_shingles = {item for set_ in shingles for item in set_[1]}
+
+    vocab = {}
+    for i, shingle in enumerate(list(all_shingles)):
+        vocab[shingle] = i
+
+    del all_shingles
+
+    # %%
+    # LSH
+    buckets = 15
+    lsh = LSH(buckets)
+
+    # %%
+    #one_hot = one_hot_encoding(vocab)
+    hash_function_count = 150
+    arr = gen_minhash(vocab, hash_function_count)
+    for id, shingle in shingles:
+        ohe = one_hot(shingle, vocab)
+
+        fingerprint = get_fingerprint(arr, ohe)
+        lsh.hash(fingerprint, id)
+
+    # %%
+    # candidate pairs
+    return list(lsh.get_candidate_pairs())
+
+
+if __name__ == '__main__':
+
+    # %%
+    X1_df['id_title'] = X1_df['id'].astype(str) + ' ' + X1_df['title']
+    X1 = X1_df['id_title'].tolist()
+    X2_df['id_title'] = X2_df['id'].astype(str) + ' ' + X2_df['name']
+    X2 = X2_df['id_title'].tolist()  # TODO: include other attributes!
+
+    X1_candidate_pairs = blocking_step(X1)
+    X2_candidate_pairs = blocking_step(X2)
+    pdb.set_trace()
+
+    # save results
+    save_output(X1_candidate_pairs, X2_candidate_pairs)
--- lsh.py
+++ lsh.py
@@ -1,64 +1,20 @@
+'''
+Implementation inspired by:
+https://www.pinecone.io/learn/locality-sensitive-hashing/
+'''
 from itertools import combinations
-import pandas as pd
 #  from tqdm import tqdm
 import numpy as np
-import time
-import logging
-import sys
 import pdb
 
 
-#  X1_df = pd.read_csv("X1_large.csv")
-#  X2_df = pd.read_csv("X2_large.csv")
-X1_df = pd.read_csv("X1.csv")
-X2_df = pd.read_csv("X2.csv")
-#  l = logging.Logger("")
-#  h = logging.StreamHandler()
-#  f = logging.Formatter(fmt="[{filename}:{lineno}] {msg}", style="{")
-#  h.setFormatter(f)
-#  l.addHandler(h)
-
-
-# %%
-def one_hot(shingles: set, vocab: dict) -> np.ndarray:
-    vec = np.zeros(len(vocab))
-    for shingle in shingles:
-        idx = vocab[shingle]
-        vec[idx] = 1
-    return vec
-
-
-# %%
-def k_shingles(data: str, k: int) -> set:
-    shingles = []
-
-    for i in range(len(data) - k + 1):
-        shingles.append(data[i:i+k])
-
-    return set(shingles)
-
-
-# %%
-def gen_minhash(vocab: dict, count: int) -> np.ndarray:
-    arr = np.zeros((count, len(vocab.keys())))
-
-    for i in range(count):
-        permutation = np.random.permutation(len(vocab)) + 1
-        arr[i, :] = permutation.copy()
-
-    return arr.astype(int)
-
-
-# %%
-def get_fingerprint(minhash, ohe):
-    idx = np.nonzero(ohe)[0].tolist()
-    shingles = minhash[:, idx]
-    fingerprint = np.min(shingles, axis=1)
-    return fingerprint
-
-
-# %%
 class LSH:
+    '''
+    Splits fingerprints into fixed amount of bands.
+    A single collision in a band is enough to produce a candidate pair.
+    Fingerprint size has to be multiple of band amount.
+    :param `b`: Amount of bands/buckets.
+    '''
 
     def __init__(self, b):
         self.bucket_count = b
@@ -66,17 +22,29 @@ class LSH:
         [self.buckets.append({}) for _ in range(b)]
 
     def gen_bands(self, fingerprint):
+        '''
+        Splits a single fingerprint into `bucket_count` many bands of same
+        size.
+        '''
         fingerprint_size = len(fingerprint)
         assert fingerprint_size % self.bucket_count == 0
         band_size = int(fingerprint_size / self.bucket_count)
 
-        # break fingerprint into bands
         bands = []
         for i in range(0, fingerprint_size, band_size):
             bands.append(fingerprint[i:i+band_size])
         return np.stack(bands)
 
     def hash(self, fingerprint, id):
+        '''
+        After generint bands for the fingerprint we 'hash' the
+        band, in our case there is no additional hash function, we simply
+        convert the band into a string representation to allow for easier
+        handling.
+        Band number is the same as bucket id.
+        :param `fingerprint`: Fingerprint to hash.
+        :param `id`: Id of row in input dataset.
+        '''
         bands = self.gen_bands(fingerprint).astype(str)
         for i, band in enumerate(bands):
             band = ','.join(band)
@@ -85,6 +53,13 @@ class LSH:
             self.buckets[i][band].append(id)
 
     def get_candidate_pairs(self):
+        '''
+        Returns all possible candidate pairs with the previously hashed
+        fingerprints.
+        For each bucket, if there's more than 1 id insides we have a collision,
+        e.g., possible candidate.
+        :returns: Set of all possible candidates.
+        '''
         candidates = []
         for bucket_band in self.buckets:
             keys = bucket_band.keys()
@@ -97,96 +72,67 @@ class LSH:
         return set(candidates)
 
 
-def save_output(X1_candidate_pairs,
-                X2_candidate_pairs):  # save the candset for both datasets to a SINGLE file output.csv
-    expected_cand_size_X1 = 1000000
-    expected_cand_size_X2 = 2000000
-
-    # make sure to include exactly 1000000 pairs for dataset X1 and 2000000 pairs for dataset X2
-    if len(X1_candidate_pairs) > expected_cand_size_X1:
-        X1_candidate_pairs = X1_candidate_pairs[:expected_cand_size_X1]
-    if len(X2_candidate_pairs) > expected_cand_size_X2:
-        X2_candidate_pairs = X2_candidate_pairs[:expected_cand_size_X2]
-
-    # make sure to include exactly 1000000 pairs for dataset X1 and 2000000 pairs for dataset X2
-    if len(X1_candidate_pairs) < expected_cand_size_X1:
-        X1_candidate_pairs.extend(
-            [(0, 0)] * (expected_cand_size_X1 - len(X1_candidate_pairs)))
-    if len(X2_candidate_pairs) < expected_cand_size_X2:
-        X2_candidate_pairs.extend(
-            [(0, 0)] * (expected_cand_size_X2 - len(X2_candidate_pairs)))
-
-    # make sure to have the pairs in the first dataset first
-    all_cand_pairs = X1_candidate_pairs + X2_candidate_pairs
-    output_df = pd.DataFrame(all_cand_pairs, columns=[
-                             "left_instance_id", "right_instance_id"])
-    # In evaluation, we expect output.csv to include exactly 3000000 tuple pairs.
-    # we expect the first 1000000 pairs are for dataset X1, and the remaining pairs are for dataset X2
-    output_df.to_csv("output.csv", index=False)
+# %%
+def one_hot(shingles: set, vocab: dict) -> np.ndarray:
+    '''
+    One-hot encodes a shingle set for a given row of the data set.
+    For every shingle in the vocab we check if the shingle is present
+    in the shingles for the row, if so -> 1, else 0.
+    :param `shingles`: Input shingle set for a single row.
+    :param `vocab`: Shingle set containing all unique shingles for all rows.
+    :returns: One hot encoded vector.
+    '''
+    vec = np.zeros(len(vocab))
+    for shingle in shingles:
+        idx = vocab[shingle]
+        vec[idx] = 1
+    return vec
 
 
-def blocking_step(X):
-    # TODO: split into #hyperthreads jobs
-
-    # %%
-    k = 3  # ~5 for small docs (emails), 9 - 10 for large docs(papers)
+# %%
+def k_shingles(data: str, k: int) -> set:
+    '''
+    Creates a k-shingle set for the input data.
+    Basically moves a sliding window of size k over the input data and creates
+    a set of all the shingles present.
+    :param `data`: Input string (one per row).
+    :param `k`: Fixed size of each shingle.
+    :returns: Set of shingles.
+    '''
     shingles = []
-    for row in X:
-        id, _, data = row.partition(' ')
-        shingles.append((id, k_shingles(data, k)))
 
-    # %%
-    all_shingles = {item for set_ in shingles for item in set_[1]}
+    for i in range(len(data) - k + 1):
+        shingles.append(data[i:i+k])
 
-    vocab = {}
-    for i, shingle in enumerate(list(all_shingles)):
-        vocab[shingle] = i
-
-    del all_shingles
-
-    # %%
-    # LSH
-    buckets = 15
-    lsh = LSH(buckets)
-
-    # %%
-    #one_hot = one_hot_encoding(vocab)
-    hash_function_count = 150
-    arr = gen_minhash(vocab, hash_function_count)
-    for id, shingle in shingles:
-        ohe = one_hot(shingle, vocab)
-
-        fingerprint = get_fingerprint(arr, ohe)
-        lsh.hash(fingerprint, id)
-
-    # %%
-    # candidate pairs
-    return list(lsh.get_candidate_pairs())
+    return set(shingles)
 
 
-def duplicate_with_new_id(X, times_more: int):
-    X_new = X
-    for _ in range(0, times_more):
-        X_new = pd.concat([X_new, X])
+# %%
+def gen_minhash(vocab: dict, count: int) -> np.ndarray:
+    '''
+    Generates `count` many minhash hashing functions/permutations.
+    :param `vocab`: Shingle set containing all unique shingles for all rows.
+    :param `count`: How many hash functions we want -> size of fingerprint.
+    :returns: Array of `count` minhash functions.
+    '''
+    arr = np.zeros((count, len(vocab.keys())))
 
-    return X_new.reset_index()
+    for i in range(count):
+        permutation = np.random.permutation(len(vocab)) + 1
+        arr[i, :] = permutation.copy()
+
+    return arr.astype(int)
 
 
-if __name__ == '__main__':
-
-    # %%
-    #  start = time.time()
-    X1_df['id_title'] = X1_df['id'].astype(str) + ' ' + X1_df['title']
-    X1 = X1_df['id_title'].tolist()
-    X2_df['id_title'] = X2_df['id'].astype(str) + ' ' + X2_df['name']
-    X2 = X2_df['id_title'].tolist()  # TODO: include other attributes!
-    #  print("X1 size " + str(len(X1_df)))
-    #  print("X2 size " + str(len(X2_df)))
-    X1_candidate_pairs = blocking_step(X1)
-    #  X2_candidate_pairs = blocking_step(X2)
-    X2_candidate_pairs = []
-    #
-    #  # save results
-    save_output(X1_candidate_pairs, X2_candidate_pairs)
-    #  end = time.time()
-    #  print(f"Runtime of the program is {end - start}.")
+# %%
+def get_fingerprint(minhash, ohe):
+    '''
+    Creates the fingerprint for a given one-hot encoded shingle set.
+    :param: `minhash`: Set of minhash functions.
+    :param: `ohe`: One-hot encoded shingle set.
+    :returns: Min-hashed fingerprint.
+    '''
+    nnz_indexes = np.nonzero(ohe)[0].tolist()
+    shingles = minhash[:, nnz_indexes]
+    fingerprint = np.min(shingles, axis=1)
+    return fingerprint
