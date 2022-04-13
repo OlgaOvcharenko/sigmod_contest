f4dee0e387f1118d865f4f43ecf8429b0da64386
philipportner <philipp.ortner@student.tugraz.at>
2022-04-13 20:36:09 +0200

    local recall calculation

---
+++ recall.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+
+Y1 = pd.read_csv("Y1.csv")
+Y1['left_right'] = Y1['lid'].astype(str) + Y1['rid'].astype(str)
+Y2 = pd.read_csv("Y2.csv")
+Y2['left_right'] = Y2['lid'].astype(str) + Y2['rid'].astype(str)
+#reference = Y1.append(Y2)['left_right'].values
+reference = Y1['left_right'].values
+
+
+
+output = pd.read_csv("output.csv")
+output['left_right'] = output['left_instance_id'].astype(str) + output['right_instance_id'].astype(str)
+
+output_values = output['left_right'].values
+inter = set.intersection(set(output_values), set(reference))
+
+recall = len(inter) / len(reference)
+print(f'Recall: {recall}.')
