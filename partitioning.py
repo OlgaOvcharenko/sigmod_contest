9cb4188f1dabf8202c62630b99736057d1cc9084
philipportner <philipp.ortner@student.tugraz.at>
2022-04-16 15:59:12 +0200

    limited blocksize, reusablefunc

--- partitioning.py
+++ partitioning.py
@@ -1,4 +1,6 @@
 import pdb
+import re
+from collections import defaultdict
 
 brands = [
     "dell",
@@ -24,10 +26,10 @@ cpu_brands = [
 
 models = [
     "thinkpad", "latitude", "inspiron", "vostro", "xps", "aspire", "toughbook",
-    "vivobook" "pavilion", "elitebook"]
+    "vivobook", "pavilion", "elitebook"]
 
 products = [
-    "carbon x1",  "x230", "2740p", "xmg", "aspire", "toughbook", "vivobook",
+    "x1 carbon", "x230", "2740p", "xmg", "aspire", "toughbook", "vivobook",
     "pavilion", "elitebook", "latitude"
 ]
 
@@ -61,7 +63,7 @@ class Partitioner():
             "X1_large.csv": X1_Partitioner,
             "X2_large.csv": X2_Partitioner,
             "X1.csv": X1_Partitioner,
-            "X2.csv": X1_Partitioner,
+            "X2.csv": X2_Partitioner,
         }
         return datasets[path](data)
 
@@ -92,11 +94,11 @@ class X1_Partitioner(Partitioner):
 
         candidate_pairs = set()
         for _, candidates in buckets.items():
-            if len(candidates) < 100: #skip patterns that are too common
-                for ix1, id1 in enumerate(candidates):
-                    for _, id2 in enumerate(candidates[ix1+1:]):
-                        pair = (id1, id2) if id1 < id2 else (id2, id1)
-                        candidate_pairs.add(pair)
+            #  print(_, len(candidates))
+            for ix1, id1 in enumerate(candidates):
+                for _, id2 in enumerate(candidates[ix1+1:]):
+                    pair = (id1, id2) if id1 < id2 else (id2, id1)
+                    candidate_pairs.add(pair)
 
         return list(candidate_pairs)
 
@@ -127,15 +129,50 @@ class X2_Partitioner(Partitioner):
     def __init__(self, df):
         super().__init__(df)
 
-    def _get_candidate_pairs(self) -> list:
-        group_dict = self.df.groupby(by=['brand']).groups
-        print(len(group_dict))
-        block_pairs = set()
-        for _, ixs in group_dict.items():
-            block = list(self.df.loc[ixs, 'id'])
-            for ix1, id1 in enumerate(block):
-                for _, id2 in enumerate(block[ix1+1:]):
-                    pair = (id1, id2) if id1 < id2 else (id2, id1)
-                    block_pairs.add(pair)
+    def partition_by_attribute(self, attributes, parents) -> list:
+        buckets = {k: [] for k in attributes}
 
-        return list(block_pairs)
+        already_blocked = []  # remove id to reduce candidate_pairs in future blocking step
+        for idx, row in self.df.iterrows():
+            title = row['title']
+            for k in buckets.keys():
+                if k in title and (len(parents) == 0 or parents[k] in title):
+                    #  pdb.set_trace()
+                    buckets[k].append(row.id)
+                    already_blocked.append(idx)
+                    break
+        self.df = self.df.drop(already_blocked)
+
+        candidate_pairs = set()
+        for _, candidates in buckets.items():
+            #  print(_, len(candidates))
+            if len(candidates) > 100: #skip patterns that are too common
+                candidates = candidates[:100]
+            for ix1, id1 in enumerate(candidates):
+                for _, id2 in enumerate(candidates[ix1+1:]):
+                    pair = (id1, id2) if id1 < id2 else (id2, id1)
+                    candidate_pairs.add(pair)
+
+        return list(candidate_pairs)
+
+    def partition_by_type(self) -> list:
+        return self.partition_by_attribute(types, [])
+
+    def partition_by_product(self) -> list:
+        return self.partition_by_attribute(products, [])
+
+    def partition_by_model(self) -> list:
+        return self.partition_by_attribute(models, brand_by_models)
+
+    def partition_by_brand(self) -> list:
+        return self.partition_by_attribute(brands, [])
+
+    def _get_candidate_pairs(self) -> list:
+        candidate_pairs = []
+
+        #  candidate_pairs.extend(self.partition_by_type())
+        candidate_pairs.extend(self.partition_by_product())
+        candidate_pairs.extend(self.partition_by_model())
+        candidate_pairs.extend(self.partition_by_brand())
+
+        return candidate_pairs
