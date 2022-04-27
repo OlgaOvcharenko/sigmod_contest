dcb4a50a43bca2cda9a5b38438d1f6a44321a91a
philipportner <philipp.ortner@student.tugraz.at>
2022-04-27 21:21:40 +0200

    added preprocessing and some kind of knnjoin

--- blocking.py
+++ blocking.py
@@ -1,6 +1,13 @@
 import pandas as pd
 import pdb
+import cProfile
+import pstats
+import io
+from pstats import SortKey
+import re
 
+from math import sqrt
+from queue import PriorityQueue
 from preprocessing import Preprocessor
 from lsh import *
 
@@ -9,11 +16,13 @@ from lsh import *
 #  X2_df = pd.read_csv("X2_large.csv")
 X1_df = pd.read_csv("X1.csv")
 X2_df = pd.read_csv("X2.csv")
+isX1 = True
 #  l = logging.Logger("")
 #  h = logging.StreamHandler()
 #  f = logging.Formatter(fmt="[{filename}:{lineno}] {msg}", style="{")
 #  h.setFormatter(f)
 #  l.addHandler(h)
+
 
 def save_output(X1_candidate_pairs,
                 X2_candidate_pairs):  # save the candset for both datasets to a SINGLE file output.csv
@@ -48,31 +57,105 @@ def blocking_step(df_path):
     dataset = ds.preprocess()   # TODO: split into #hyperthreads jobs
 
     k = 3  # ~5 for small docs (emails), 9 - 10 for large docs(papers)
-    shingles = []
+    if isX1:
+        K = 10  # kNN-Join K
+    else:
+        K = 5
+
+    if isX1:
+        min_sim = 0.8
+    else:
+        min_sim = 0.6
+
+    shingles = {}
+    source_freq = {}
+    index = {}
+    common_token_counter = {}
+    flags = {}
+
     for _, row in dataset.iterrows():
         data = row['title']
-        shingles.append((row['id'], k_shingles(data, k)))
+        id = row['id']
+        flags[id] = -1
+        # TODO:
+        #  shngl = k_shingles(data, 9)
+        shngl = data.split()
+        #  pdb.set_trace()
+        #  shngl = [' '.join(shngl[i: i + 2]) for i in range(0, len(shngl), 2)]
+        shingles[id] = shngl
+        source_freq[id] = len(shngl)
+        for s in shngl:
+            if s in index:
+                index[s].append(id)
+            else:
+                index[s] = [id]
 
-    all_shingles = {item for set_ in shingles for item in set_[1]}
+    cp = set()
+    i = 0
+    j = 0
+    for id, shingle_set in shingles.items():
+        local_cp = []
+        i += 1
+        # loop over all shingles for a given row
+        src_ids = []
+        for s in shingle_set:
+            src_ids.extend(index[s])
+            if not src_ids:
+                continue
 
-    vocab = {}
-    for i, shingle in enumerate(list(all_shingles)):
-        vocab[shingle] = i
+            # loop over all ids that are in the bucket of one of the shingles
+        for src_id in src_ids:
+            if src_id == id:
+                continue
+            if flags[src_id] != id:
+                common_token_counter[src_id] = 0
+                flags[src_id] = id
+            common_token_counter[src_id] = common_token_counter[src_id] + 1
+            counter = common_token_counter[src_id]
 
-    del all_shingles
+            if counter > 0:
+                pair = (id, src_id) if id < src_id else (src_id, id)
+                if pair not in cp:
+                    similarity = counter / \
+                        sqrt(source_freq[src_id] * len(shingle_set))
 
-    buckets = 15
-    lsh = LSH(buckets)
+                    if similarity >= min_sim:
+                        pair = (id, src_id) if id < src_id else (src_id, id)
+                        cp.add(pair)
 
-    hash_function_count = 150
-    arr = gen_minhash(vocab, hash_function_count)
-    for id, shingle in shingles:
-        ohe = one_hot(shingle, vocab)
+        #  if not local_cp:
+        #      continue
+        #
+        #  k_sim = PriorityQueue()
+        #  min_sim = 0
+        #  if len(local_cp) > max_local_cp:
+        #      max_local_cp = len(local_cp)
+        #
+        #  for candidate in local_cp:
+        #      j += 1
+        #      common = common_token_counter[candidate]
+        #
+        #      # cosine sim
+        #      similarity = common / \
+        #          sqrt(source_freq[candidate] * len(shingle_set))
 
-        fingerprint = get_fingerprint(arr, ohe)
-        lsh.hash(fingerprint, id)
+        #      if min_sim < similarity:
+        #          k_sim.put((-similarity, similarity))
+        #          if (K < k_sim.qsize()):
+        #              min_sim = k_sim.get()[1]
+        #
+        #  for candidate in local_cp:
+        #      common = common_token_counter[candidate]
+        #
+        #      # cosine sim
+        #      similarity = common / \
+        #          sqrt(source_freq[candidate] * len(shingle_set))
 
-    return list(lsh.get_candidate_pairs())
+            #  if similarity >= min_sim:
+            #      pair = (id, candidate) if id < candidate else (candidate, id)
+            #      cp.add(pair)
+    #  print(f"maxlen: {max_local_cp}\ni: {i}\tj: {j}")
+    return list(cp)
 
 
 def recall(true, prediction):
@@ -80,15 +163,18 @@ def recall(true, prediction):
 
 
 if __name__ == '__main__':
-
-    # %%
-    #  X1_df['id_title'] = X1_df['id'].astype(str) + ' ' + X1_df['title']
-    #  X1 = X1_df['id_title'].tolist()
-    #  X2_df['id_title'] = X2_df['id'].astype(str) + ' ' + X2_df['name']
-    #  X2 = X2_df['id_title'].tolist()  # TODO: include other attributes!
-
+    pr = cProfile.Profile()
+    pr.enable()
     X1_candidate_pairs = blocking_step("X1.csv")
+    isX1 = False
     X2_candidate_pairs = blocking_step("X2.csv")
+    pr.disable()
+    s = io.StringIO()
+
+    sortby = SortKey.CUMULATIVE
+    ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+    ps.print_stats(10)
+    print(s.getvalue())
 
     print(f'X1_candidate_pairs: {len(X1_candidate_pairs)}')
     print(f'X2_candidate_pairs: {len(X2_candidate_pairs)}')
